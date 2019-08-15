91404aa63848dd5eff2e0b79a72e38ba68e9f7b9
Jake Mathai <jakemathai5@gmail.com>
2019-08-14 17:54:21 -0700

    Update models.py

--- api/models.py
+++ api/models.py
@@ -3,17 +3,17 @@
 from django.db import models
 import datetime
 
-#Class contains each CSV column as a field
+# Contains each CSV column as a field
 class Transaction(models.Model):
 	id = models.IntegerField(default=-1, primary_key=True) 
 	user = models.IntegerField(default=-1)
 	transaction_date = models.DateField(default=datetime.date.today)
-	sales_amount = models.FloatField(default=-1.0)
+	sales_amount = models.FloatField(default=0.0)
 	join_date = models.DateField(default=datetime.date.today)
 	region = models.CharField(max_length=1)
 	class Meta:
-		db_table = 'Transactions'	#Name of DB table
-	def __str__(self):    # Command line return format
+		db_table = 'Transactions'	# DB table alias
+	def __str__(self):    # Self print appearance
 		out = str(self.num) + ', ' + str(self.user) + ', ' 
 		out += self.transaction_date.strftime('%m/%d/%Y') + ', '
 		out += str(self.sales_amount) + ', ' + self.region
